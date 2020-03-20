828b4bafcf3aed89a91c0df18ffcd222fbce5268
Bomma-Pranay <62324691+Bomma-Pranay@users.noreply.github.com>
2020-03-20 12:54:39 +0530

    Create SortLinkedList.py

---
+++ SortLinkedList.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+def middle(h,flag):
+    s,f=h,h
+    while f!=None and f.next!=None and f.next.next!=None:
+        s=s.next
+        f=f.next.next
+    if flag==True:
+        #print(s.data)
+        return s
+    #print(s.next.data)
+    return s.next
+def merge(h1, h2):
+    d = ListNode('a')
+    td=d
+    while h1 != None and h2 != None:
+        if h1.val < h2.val:
+            d.next = h1
+            h1 = h1.next
+        else:
+            d.next = h2
+            h2 = h2.next
+        d = d.next
+    if h1 != None:
+        d.next = h1
+    if h2 != None:
+        d.next = h2
+    return td.next
+def mergeSort(h):
+    if h==None or h.next==None:
+        return h
+    m=middle(h,True)
+    sh=m.next
+    m.next=None
+    return merge(mergeSort(h),mergeSort(sh))
+
+class Solution:
+    def sortList(self, h):
+        return mergeSort(h)
